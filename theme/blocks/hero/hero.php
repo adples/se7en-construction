7f7ba1d9c9323e44572c3889ab95810791b4ebff
Alec Plescia <adples.design@gmail.com>
2025-04-11 08:51:17 -0400

    adjust hero

--- theme/blocks/hero/hero.php
+++ theme/blocks/hero/hero.php
@@ -18,7 +18,7 @@ $anchor = ( empty( $block['anchor'] ) ) ? null : 'id=' . $block['anchor'];
 $additional_classes = $block['className'] ?? '';
 
 // Step default classes
-$hero_default_classes = 'relative bg-gray-400 min-h-[800px] h-screen hero-home';
+$hero_default_classes = 'relative bg-gray-400 h-screen md:min-h-[800px] hero-home';
 
 // Filter Overlay
 $rand = rand ( 10000 , 99999 );
@@ -85,7 +85,7 @@ if( get_field('bg') ){
 	<div class="grid grid-cols-24 gap-4 relative z-5">
 
 		<div class="hidden md:block md:col-span-3 md:m-auto">
-			<div class="flex [writing-mode:vertical-lr] min-h-[800px] h-screen -rotate-180 px-10 w-10">
+			<div class="flex [writing-mode:vertical-lr] h-screen md:min-h-[800px] -rotate-180 px-10 w-10">
 				<div class="flex border-l-2 border-white h-full pl-4">
 					<div class="flex-grow flex justify-center">
 						<ul class="flex 2xl:text-lg leading-5 uppercase text-white font-bold">
@@ -137,7 +137,7 @@ if( get_field('bg') ){
 
 
 		<div class="hidden md:block md:col-span-3 md:m-auto">
-			<div class="flex [writing-mode:vertical-lr] min-h-[800px] h-screen px-10 w-10">
+			<div class="flex [writing-mode:vertical-lr] h-screen md:min-h-[800px] px-10 w-10">
 				<div class="flex border-l-2 border-white h-full pl-4">
 					<div class="flex-grow flex justify-center">
 						<?php get_template_part( 'template-parts/layout/hero-nav/nav' ); ?>
