5f9c9629670d0960051d9c724ea567e781723127
Alec Plescia <adples.design@gmail.com>
2025-04-24 12:08:34 -0400

    footer

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -9,11 +9,11 @@
 
 ?>
 
-<footer id="sayHi" class="footer pt-15 xl:pt-25 pb-15 xl:pb-25 bg-filter relative overflow-hidden filter-71482 is-style-gradient-overlay-rev bg-cover bg-center bg-no-repeat">
+<footer id="sayHi" class="relative bg-filter bg-cover bg-no-repeat bg-center md:bg-fixed bg-local filter-71482 pt-15 xl:pt-25 pb-15 xl:pb-25 overflow-hidden footer is-style-gradient-overlay-rev">
 
 	<?php if(!is_page( 'contact' )): ?>
-	<div class="relative z-2 mx-auto px-6 container">
-		<div class="px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16 relative bg-white rounded-[1rem] md:rounded-[2rem] lg:rounded-[3rem] !rounded-tl-none shadow-(--subtle-shadow)">
+	<div class="z-2 relative mx-auto px-6 container">
+		<div class="relative bg-white shadow-(--subtle-shadow) px-4 md:px-8 lg:px-12 py-8 md:py-12 lg:py-16 rounded-[1rem] md:rounded-[2rem] lg:rounded-[3rem] !rounded-tl-none">
 			<?php get_template_part( 'template-parts/components/badge', null, array('text' => 'Get In Touch') );?>
 
 			<?php if( get_field('footer_title','option') ): ?>
@@ -21,17 +21,17 @@
 			<?php endif; ?>
 
 			<?php if( get_field('footer_lead','option') ): ?>
-				<p class="pt-8 2xl:text-lg uppercase italic"><?php echo get_field('footer_lead','option') ?></p>
+				<p class="pt-8 2xl:text-lg italic uppercase"><?php echo get_field('footer_lead','option') ?></p>
 			<?php endif; ?>
 
 			<div class="flex lg:flex-row flex-col items-center gap-12 lg:gap-6 mt-12">
-				<div class="w-full lg:order-1 lg:basis-7/12 xl:basis-1/2">
-					<div class="p-4 md:p-8 lg:p-12 relative bg-white rounded-[1rem] md:rounded-[2rem] lg:rounded-[3rem] border-2 border-foreground !rounded-br-none">
+				<div class="lg:order-1 w-full lg:basis-7/12 xl:basis-1/2">
+					<div class="relative bg-white p-4 md:p-8 lg:p-12 border-2 border-foreground rounded-[1rem] md:rounded-[2rem] lg:rounded-[3rem] !rounded-br-none">
 						<?php echo do_shortcode( '[gravityform id="1" title="false"]' ); ?>
 					</div>
 				</div>
 
-				<div class="w-full basis-full sm:flex lg:basis-5/12 xl:basis-1/2">
+				<div class="sm:flex w-full basis-full lg:basis-5/12 xl:basis-1/2">
 					<?php get_template_part( 'template-parts/components/contact-info' ); ?>
 				</div>
 			</div>
@@ -39,22 +39,22 @@
 	</div>
 	<?php endif; ?>
 	<div class="pt-12">
-		<div class="relative overflow-hidden z-2 spotlight">
+		<div class="z-2 relative overflow-hidden spotlight">
 		<div>
 			<ul class="flex justify-center md:justify-start items-center [&_li]:mx-8 [&_img]:max-w-none">
 				<?php for ($x = 0; $x <= 10; $x++): ?>
 					<li>
-						<span class="font-serif text-[6rem] lg:text-[8rem] italic uppercase text-white/33 noselect">Se7en</span>
+						<span class="font-serif text-[6rem] text-white/33 lg:text-[8rem] italic uppercase noselect">Se7en</span>
 					</li>
 				<?php endfor; ?>
 			</ul>
 		</div>
 
-		<div data-se7en class="absolute top-0 left-0 w-full spotlight-mask">
+		<div data-se7en class="top-0 left-0 absolute w-full spotlight-mask">
 			<ul class="flex justify-center md:justify-start items-center [&_li]:mx-8 [&_img]:max-w-none">
 				<?php for ($x = 0; $x <= 10; $x++): ?>
 					<li>
-						<span class="font-serif text-[6rem] lg:text-[8rem] italic uppercase text-white noselect">Se7en</span>
+						<span class="font-serif text-[6rem] text-white lg:text-[8rem] italic uppercase noselect">Se7en</span>
 					</li>
 				<?php endfor; ?>
 			</ul>
@@ -65,11 +65,11 @@
 		</div>
 	</div>
 
-	<div class="mx-auto px-6 container relative z-2">
+	<div class="z-2 relative mx-auto px-6 container">
 		<div class="flex flex-row justify-center mt-6">
-			<div class="w-full basis-full lg:flex lg:basis-6/12">
-				<div class="w-full py-6 border-y border-y-white/25 text-center">
-					<ul class="md:flex md:justify-center  text-white/75 text-sm">
+			<div class="lg:flex w-full basis-full lg:basis-6/12">
+				<div class="py-6 border-y border-y-white/25 w-full text-center">
+					<ul class="md:flex md:justify-center text-white/75 text-sm">
 						<li class="md:px-3 md:border-e md:border-white/25 last:border-0">&copy; <?php echo date("Y").' '.get_bloginfo().' All Rights Reserved.'; ?></li>
 						<li class="md:px-3 md:border-e md:border-white/25 last:border-0"><a href="<?php echo esc_url(get_permalink(3)) ?>">Privacy Policy</a></li>
 					</ul>
